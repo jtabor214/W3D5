0141d9b65dad74e0e72a0c5aa5ce7c374507e15b
Jacob Tabor <jtabor214@gmail.com>
2023-11-03 14:31:06 -0400

    Completed #initialize and #value, #parent, #children, and #parent=

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,24 @@
 class PolyTreeNode
+    
+    attr_reader :value, :parent, :children
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end   
+
+  def parent=(parent_node)
+        #return self if self.parent == parent_node
+        #deattach from the current parent so we can reassign to the new parent
+        if self.parent
+            self.parent.children.delete(self)
+        end
+
+        @parent = parent_node
+        self.parent.children << self unless self.parent.nil?
+  end  
   
+
+
 end
